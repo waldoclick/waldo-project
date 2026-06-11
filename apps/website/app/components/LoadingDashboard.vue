eec60978e1f38eba700945a56cb943c3c09202be
Gabriel Burgos <waldoclick@users.noreply.github.com>
2026-06-11 18:44:18 -0400

    chore(quick-260611-pyz): delete 19 orphaned dashboard-migration components
    Remove dead code left over from Phase 125 dashboard-into-website merge:
    - FormLoginDashboard, FormForgotPasswordDashboard, FormResetPasswordDashboard,
      FormVerifyCodeDashboard, IntroduceAuthDashboard (auth variants replaced by website versions)
    - LightboxAdblockDashboard, LightboxCookiesDashboard (replaced by website versions)
    - AvatarDashboard, LoadingDashboard, FormDevDashboard, LogoWhiteDashboard,
      PictureDashboard (UI stubs replaced by website versions)
    - AccountPacks, AdArchiveProfile, BarResume, MapDefault, ResumeAd,
      SellerContact, ThanksDefault (legacy components not used anywhere)

--- apps/website/app/components/LoadingDashboard.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="loading loading--default">
-    <IconLoader2 :size="32" class="loading--default__icon" />
-  </div>
-</template>
-
-<script setup>
-import { Loader2 as IconLoader2 } from "lucide-vue-next";
-</script>
